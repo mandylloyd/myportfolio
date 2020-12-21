31f5777a6de8beb0e66f1d27ddf8d19096fe0b63
honeybee <63674362+mandylloyd@users.noreply.github.com>
2020-12-21 12:56:32 -0500

    adding about me page, image not showing up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 // // import PortfolioWork from './components/PortfolioWork';
-// import AboutMe from './components/AboutMe';
+import AboutMe from './components/AboutMe';
 import './index.css';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 
@@ -12,9 +12,9 @@ const App = () => {
         <div>
             <Router>
                 <Navbar/>
-                {/* <Route path="/about"
+                <Route path="/about"
                     component={AboutMe}/>
-                <Route path="/portfolio"
+                {/*<Route path="/portfolio"
                     component={PortfolioWork}/>
                 <Route path="/contact"
                     component={ContactForm}/> */}
