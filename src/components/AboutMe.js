f1b433744bf0e94d6a5554de78aff35c2df8466b
honey bee <honeybee@users.noreply.github.com>
2020-12-21 16:47:27 -0500

    fixed image on about me and added description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
     return (
         <div>
             <div className="ui text container">
-                <img alt="me" src={require('../images/me.png')}/>
+                <img alt="me" className="ui large circular image center" src="../images/me.png"/>
               
 
                 <div className="ui list">
@@ -41,6 +41,9 @@ const AboutMe = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className="aboutme"> {'\t'} I studied Strategic Advertising at Virginia Commonwealth University. After graduating in 2017 with a Bachelors of Science in Mass Communication I have spent my time working in the service industry. I picked up an interest in User Experience along the way and am now pursuing knowledge of Web Development. I completed a Coding Bootcamp through The University of Richmond where I acquired a list of full stack skills. I'm looking to expand these skills and dive deeper into langauges like React and JavaScript to further my career as a Junior Softare Developer. 
+</div>
 
             </div>
         </div>
